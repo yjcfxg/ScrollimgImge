5c695173ae7f7d23eebf7e4e14c67383ade29a6e
yjcfxg <yjcfxg@163.com>
2017-02-24 17:23:10 +0800

    Initial Commit

---
+++ ScrollingImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollingImage
+//
+//  Created by yjcfxg on 2017/2/24.
+//  Copyright © 2017年 yjcfxg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollingImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollingImage
+//
+//  Created by yjcfxg on 2017/2/24.
+//  Copyright © 2017年 yjcfxg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
